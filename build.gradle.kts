63b351e68f35ef51d17f5ff9b5fca2299bea37ae
nekooooooooo <69033860+nekooooooooo@users.noreply.github.com>
2025-04-09 16:23:57 +0800

    Change modImplementation to compileOnly for MC2Discord and DiscordIntegration dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,11 +70,11 @@ dependencies {
     // modImplementation("maven.modrinth:dcintegration:${property("dcintegration_version")}")
 
     // MC2Discord
-    modImplementation("maven.modrinth:mc2discord:Gl8sM8TE")
+    compileOnly("maven.modrinth:mc2discord:Gl8sM8TE")
 
     // DiscordIntegration
     // compileOnly("de.erdbeerbaerlp:dcintegration.common:3.0.7")
-    modImplementation("de.erdbeerbaerlp:dcintegration.common:3.0.7")
+    compileOnly("de.erdbeerbaerlp:dcintegration.common:3.0.7")
     compileOnly("org.apache.logging.log4j:log4j-api:2.17.1")
 }
 
