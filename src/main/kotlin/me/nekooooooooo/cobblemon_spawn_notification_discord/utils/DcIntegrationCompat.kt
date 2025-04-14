c4eda805ef56628f463fb2d27c0142a7e417f617
nekooooooooo <69033860+nekooooooooo@users.noreply.github.com>
2025-04-14 18:01:35 +0800

    Add dcintegration webhook support

--- src/main/kotlin/me/nekooooooooo/cobblemon_spawn_notification_discord/utils/DcIntegrationCompat.kt
+++ src/main/kotlin/me/nekooooooooo/cobblemon_spawn_notification_discord/utils/DcIntegrationCompat.kt
@@ -1,9 +1,17 @@
 package me.nekooooooooo.cobblemon_spawn_notification_discord.utils
 
 import de.erdbeerbaerlp.dcintegration.common.DiscordIntegration
+import me.nekooooooooo.cobblemon_spawn_notification_discord.config.ConfigManager
+import de.erdbeerbaerlp.dcintegration.common.storage.Configuration
 
 object DcIntegrationCompat {
     fun send(message: String) {
-        DiscordIntegration.INSTANCE?.sendMessage(message)
+        if(Configuration.instance().webhook.enable) {
+            DiscordIntegration.INSTANCE?.sendMessage(
+                message,
+                ConfigManager.CONFIG.botAvatarUrl,
+                ConfigManager.CONFIG.botName
+            )
+        } else DiscordIntegration.INSTANCE?.sendMessage(message)
     }
 }
